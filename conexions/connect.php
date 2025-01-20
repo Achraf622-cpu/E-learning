190990910703f1cc6e13fad2cb7fcd6130d57994
Achraf <hanzazachraf581@gmail.com>
2025-01-20 10:24:37 +0100

    changed the connection

--- conexions/connect.php
+++ conexions/connect.php
@@ -2,7 +2,7 @@
 $host = '127.0.0.1';
 $user = 'root';
 $pass = 'password';
-$database = 'OOP';
+$database = 'elearn';
 
 try {
     $conn = new PDO("mysql:host={$host};dbname={$database}", $user, $pass);
